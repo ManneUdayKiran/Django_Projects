56c177e39ae4288fc42f58414eb0b5d2b301b4ae
MANNE UDAY KIRAN <143863214+ManneUdayKiran@users.noreply.github.com>
2025-05-31 21:38:39 +0530

    Update settings.py

--- Todo_App/settings.py
+++ Todo_App/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ec-5g&nkqy7+ux_gs3n^%njuc8eb71#=+*50-gc$m%x5u=n0uh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-projects-ed78.onrender.com', 'localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",  # React's dev server
